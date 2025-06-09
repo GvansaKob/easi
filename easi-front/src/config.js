c83cb92189060d863329ad90d3e16deeba7ab963
GvansaKobesashvili <gvansavibly@gmail.com>
2025-06-09 19:54:28 +0200

    activate mode standalone (manifest) + Service Worker

--- easi-front/src/config.js
+++ easi-front/src/config.js
@@ -1,1 +1,4 @@
-export const API_URL = 'http://localhost:3000';
+export const API_URL =
+  window.location.hostname === 'localhost'
+    ? 'http://192.168.1.146:3000'
+    : 'http://192.168.1.146:3000'
