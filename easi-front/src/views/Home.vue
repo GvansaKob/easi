517ae1939ba9346e14ffcc25edea02d2b39c911e
GvansaKobesashvili <gvansavibly@gmail.com>
2025-06-09 20:47:15 +0200

    add header to homepage

--- easi-front/src/views/Home.vue
+++ easi-front/src/views/Home.vue
@@ -1,5 +1,15 @@
+<script setup>
+import Header from '@/components/Header.vue'
+</script>
+
 <template>
-  <div class="p-6">
-    <h1 class="text-2xl font-bold text-violet">Bienvenue sur EASI</h1>
-  </div>
+    <div class="bg-blanc min-h-screen">
+        <Header />
+        <main class="p-4">
+            <!-- contenu -->
+        </main>
+    </div>
+    <div class="p-6">
+        <h1 class="text-2xl font-bold text-violet">Bienvenue sur EASI</h1>
+    </div>
 </template>
