370a5152584ad2efc8733995aa3c7f2f2bda2a59
GvansaKobesashvili <gvansavibly@gmail.com>
2025-06-11 14:12:44 +0200

    import icones Bottom Nav

--- easi-front/src/main.js
+++ easi-front/src/main.js
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/main.css' // ou './main.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 
 
 const app = createApp(App)
