322249167e131abb0dfe4687f9e3e2f2646d13f3
GvansaKobesashvili <gvansavibly@gmail.com>
2025-06-12 18:18:56 +0200

    carte aides cliquable + routing vers diff pages

--- easi-front/src/components/CarteAide.vue
+++ easi-front/src/components/CarteAide.vue
@@ -1,14 +1,16 @@
 <template>
-  <div
-    class="bg-gray-100 rounded-2xl p-4 h-[160px] shadow-md overflow-hidden"
-  >
-    <h3 class="text-violet font-titre font-bold text-lg mb-2">
-      {{ aide.titre }}
-    </h3>
-    <p class="text-violet/80 text-sm font-textse leading-snug line-clamp-3">
-      {{ aide.description }}
-    </p>
-  </div>
+  <router-link :to="`/aide/${aide.id}`" class="block group">
+    <div
+      class="bg-gray-100 rounded-2xl p-4 h-[145px] shadow-md overflow-hidden transition duration-300 group-hover:bg-violet group-hover:text-white"
+    >
+      <h3 class="text-violet font-titre font-bold text-lg mb-2 group-hover:text-white">
+        {{ aide.titre }}
+      </h3>
+      <p class="text-violet/80 text-sm font-textse leading-snug line-clamp-3 group-hover:text-white">
+        {{ aide.description }}
+      </p>
+    </div>
+  </router-link>
 </template>
 
 <script setup>
