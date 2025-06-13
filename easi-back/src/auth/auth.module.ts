8bdbcbabcdcafa62b382843ae28a6344fb51b883
GvansaKobesashvili <gvansavibly@gmail.com>
2025-06-14 01:05:17 +0200

    sécu typage clé JWT - JWTStrategy

--- easi-back/src/auth/auth.module.ts
+++ easi-back/src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
@@ -20,7 +21,7 @@ import { UserModule } from 'src/user/user.module';
       }),
     }),
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
   exports: [AuthService],
 })
