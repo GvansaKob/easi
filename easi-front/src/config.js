f032f8090bf010f609ebc47163a3f14badb57a04
GvansaKobesashvili <gvansavibly@gmail.com>
2025-06-16 23:19:11 +0200

    API_URL via  .env pour que IP locale -> séco

--- easi-front/src/config.js
+++ easi-front/src/config.js
@@ -1,4 +1,1 @@
-export const API_URL =
-  window.location.hostname === 'localhost'
-    ? 'http://192.168.1.146:3000'
-    : 'http://192.168.1.146:3000'
+export const API_URL = import.meta.env.VITE_API_URL;
