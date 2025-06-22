ea31dd7596a4826358b46fdb27445ddd4e72ceea
GvansaKobesashvili <gvansavibly@gmail.com>
2025-06-22 20:10:11 +0200

    création composant BlocAide

---
+++ easi-front/src/components/BlocAide.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex flex-col items-center gap-4 bg-green-100 rounded-lg p-4 w-full">
+    <div class="flex flex-row gap-4 items-center">
+      <img
+        src="/images/blocaide.png"
+        alt="Vincent"
+        class="w-50 h-50 object-cover rounded-md shadow"
+      />
+      <div class="text-sm text-left">
+        <h2 class="text-2xl font-titre font-bold text-violet mb-1">Besoin d’aides ?</h2>
+        <p>Vincent peut vous guider dans vos démarches !</p>
+        <p class="mt-2 text-xs text-black/60">
+          L’appel coûte 2 €, peu importe le temps passé, mais le contact par mail reste 100 % gratuit.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
