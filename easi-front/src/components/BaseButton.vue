d94d96e739d13838e320c669169c4569adf91aff
GvansaKobesashvili <gvansavibly@gmail.com>
2025-06-22 20:09:33 +0200

    création composant BaseButton.vue

---
+++ easi-front/src/components/BaseButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button
+    class="w-80 mx-auto block py-3 px-4 rounded-lg bg-violet text-white font-bold text-center active:bg-vert transition duration-200"
+    @click="onClick"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup>
+const emit = defineEmits(['click'])
+
+function onClick(event) {
+  emit('click', event)
+}
+</script>
