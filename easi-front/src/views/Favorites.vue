8b600f9bee4aed007a2ce549c0454ba927657e9e
GvansaKobesashvili <gvansavibly@gmail.com>
2025-06-23 23:49:56 +0200

    création page Favoris.vue

---
+++ easi-front/src/views/Favorites.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="p-6 text-center">
+    <h1 class="text-2xl font-bold text-violet font-titre mb-6">Mes Favoris</h1>
+
+    <div v-if="!isLoggedIn" class="mt-10">
+      <p class="text-lg text-black mb-4">Tu n’as pas encore d’aides.</p>
+      <p class="text-black mb-6">Connecte toi pour en ajouter !</p>
+      <button
+        class="bg-violet text-white text-lg font-semibold px-6 py-2 rounded-xl shadow-md"
+        @click="router.push('/login')"
+      >
+        Se connecter
+      </button>
+    </div>
+
+    <div v-else-if="aides.length === 0" class="text-gray-500 italic mt-10">
+      Vous n'avez pas encore de favoris.
+    </div>
+
+    <div v-else class="flex flex-col gap-6 mt-6">
+      <CarteAide v-for="aide in aides" :key="aide.id" :aide="aide.aide" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import CarteAide from '@/components/CarteAide.vue'
+import { authService } from '@/services/authService'
+import { useRouter } from 'vue-router'
+import { API_URL } from '@/config'
+
+
+const router = useRouter()
+const aides = ref([])
+const isLoggedIn = authService.isLoggedIn()
+
+onMounted(async () => {
+  const userId = authService.getUserId()
+  if (!userId) return
+  const res = await fetch(`${API_URL}/favoris/${userId}`)
+  if (res.ok) {
+    aides.value = await res.json()
+  }
+})
+</script>
