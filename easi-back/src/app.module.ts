fdc8b596ae42ed8d3bf48e60467f153f72408ed6
GvansaKobesashvili <gvansavibly@gmail.com>
2025-06-25 11:38:36 +0200

    app.module modif aiven settings

--- easi-back/src/app.module.ts
+++ easi-back/src/app.module.ts
@@ -10,20 +10,18 @@ import { FavoriModule } from './favori/favori.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        type: 'mysql',
-        host: process.env.DB_HOST,
-        port: parseInt(process.env.DB_PORT || '3306', 10),
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        autoLoadEntities: true,
-        synchronize: true,
-        logging: true,
-
-      }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '3306', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,
+      ssl: true,
+      logging: true, 
+      autoLoadEntities: true,
     }),
 
     AideModule,
